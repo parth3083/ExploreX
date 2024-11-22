0b6e4fac85b6b39413f2ece1b1ddd4a93094855b
parth3083 <parthbrajput30@gmail.com>
2024-11-22 20:15:26 +0530

    Carousel added

--- client/src/components/Home Page/HeroSection.tsx
+++ client/src/components/Home Page/HeroSection.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Searchbar from './Searchbar'
+import React from "react";
+import Searchbar from "./Searchbar";
+import CarouselSection from "./CarouselSection";
 
 function HeroSection() {
   return (
-      <div className='w-full min-h-screen  pt-5 md:pt-10 lg:pt-10'>
-          <Searchbar/>
-      </div>
-  )
+    <div className="w-full min-h-screen flex flex-col items-center gap-10 pt-5 md:pt-10 lg:pt-10">
+      <Searchbar />
+      <CarouselSection />
+    </div>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
