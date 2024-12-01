8200a15d2c9dbc68e520015251b2886ced99b037
DOMinator.jsx <parthbrajput30@gmail.com>
2024-12-01 17:39:14 +0530

    Created the news model

---
+++ client/src/models/news.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema, Model, Document } from "mongoose";
+
+interface Inews extends Document {
+  todaysNews: [];
+  thisWeeksNews: [];
+  thisMonthsNews: [];
+  lastUpdated: Date;
+}
+
+const newSchema: Schema = new Schema({
+  todaysNews: [
+    {
+      title: String,
+      imageUrl: String,
+    },
+  ],
+  thisWeeksNews: [
+    {
+      title: String,
+      imageUrl: String,
+    },
+  ],
+  thisMonthsNews: [
+    {
+      title: String,
+      imageUrl: String,
+    },
+  ],
+  lastUpated: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const NewsModel: Model<Inews> =
+  mongoose.models.mews || mongoose.model<Inews>("news", newSchema);
+export default NewsModel;
